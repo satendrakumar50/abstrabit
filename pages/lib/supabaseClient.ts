365326676330709e6cdaa283e52578d241836380
satendrakumar50 <gk1293123@gmail.com>
2026-02-19 21:47:34 +0530

    some changes

---
+++ pages/lib/supabaseClient.ts
@@ -0,0 +1,23 @@
+// import { createBrowserClient } from '@supabase/ssr'
+
+// export const supabase = createBrowserClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// )
+
+
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+
+
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
